7de4bb6d501fd4047e7398f6657e039476c899d3
Fred Yankowski <fcy@imsa.edu>
2016-06-30 15:13:49 -0500

    Add Component4

---
+++ Component4.elm
@@ -0,0 +1,60 @@
+module Component4 exposing (Model, Msg(ClearNotify), init, update, view, notifying)
+
+import Html as H exposing (Html, Attribute)
+import Html.Attributes as HA
+import Html.Events as HE
+
+
+type alias Model =
+    { count : Int
+    , notify : Bool
+    }
+
+
+init : Model
+init =
+    Model 0 False
+
+
+type Msg
+    = Notify
+    | Increment
+    | ClearNotify
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            { model | count = model.count + 1, notify = False } ! []
+
+        Notify ->
+            { model | notify = True } ! []
+
+        ClearNotify ->
+            { model | notify = False } ! []
+
+
+notifying : Model -> Bool
+notifying model =
+    model.notify
+
+
+view : Model -> Html Msg
+view model =
+    H.div []
+        [ H.button [ HE.onClick Increment ] [ H.text "+" ]
+        , H.div [ countStyle ] [ H.text (toString model) ]
+        , H.button [ HE.onClick Notify ] [ H.text "Notify" ]
+        ]
+
+
+countStyle : Attribute Msg
+countStyle =
+    HA.style
+        [ ( "font-size", "20px" )
+        , ( "font-family", "monospace" )
+        , ( "display", "inline-block" )
+        , ( "width", "50px" )
+        , ( "text-align", "center" )
+        ]
--- Main.elm
+++ Main.elm
@@ -6,6 +6,7 @@ import Html.Attributes as HA
 import Component1
 import Component2
 import Component3
+import Component4
 
 
 type alias Model =
@@ -15,6 +16,8 @@ type alias Model =
     , notified2 : Bool
     , component3 : Component3.Model
     , notified3 : Bool
+    , component4 : Component4.Model
+    , notified4 : Bool
     }
 
 
@@ -22,6 +25,7 @@ type Msg
     = Component1Msg Component1.Msg
     | Component2Msg Component2.Msg
     | Component3Msg Component3.Msg
+    | Component4Msg Component4.Msg
 
 
 main : Program Never
@@ -36,7 +40,7 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model Component1.init False Component2.init False Component3.init False
+    ( Model Component1.init False Component2.init False Component3.init False Component4.init False
     , Cmd.none
     )
 
@@ -68,16 +72,38 @@ update msg model =
                     in
                         { model | component2 = c2model } ! [ Cmd.map Component2Msg c2cmd ]
 
-
         Component3Msg c3Msg ->
             let
                 ( c3model, c3cmd, notified ) =
                     Component3.update c3Msg model.component3
+
                 notified' =
-                    if notified then not model.notified3 else model.notified3
+                    if notified then
+                        not model.notified3
+                    else
+                        model.notified3
             in
                 ( { model | component3 = c3model, notified3 = notified' }, Cmd.map Component3Msg c3cmd )
-            
+
+        Component4Msg c4Msg ->
+            let
+                ( c4model, c4cmd ) =
+                    Component4.update c4Msg model.component4
+
+                notified' =
+                    if Component4.notifying c4model then
+                        not model.notified4
+                    else
+                        model.notified4
+
+                ( c4model', c4cmd' ) =
+                    if Component4.notifying c4model then
+                        Component4.update Component4.ClearNotify c4model
+                    else
+                        ( c4model, Cmd.none )
+            in
+                ( { model | component4 = c4model', notified4 = notified' },
+                      Cmd.batch [ Cmd.map Component4Msg c4cmd, Cmd.map Component4Msg c4cmd' ] )
 
 
 view : Model -> Html.Html Msg
@@ -89,6 +115,8 @@ view model =
             [ Component2.view model.component2 |> Html.App.map Component2Msg ]
         , Html.div [ HA.classList [ ( "notified", model.notified3 ) ] ]
             [ Component3.view model.component3 |> Html.App.map Component3Msg ]
+        , Html.div [ HA.classList [ ( "notified", model.notified4 ) ] ]
+            [ Component4.view model.component4 |> Html.App.map Component4Msg ]
         ]
 
 
