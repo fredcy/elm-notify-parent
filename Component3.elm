68c8b80a4140b55583cf8225558eb4f2a7c427b2
Fred Yankowski <fcy@imsa.edu>
2016-06-30 12:10:28 -0500

    add Component3

---
+++ Component3.elm
@@ -0,0 +1,49 @@
+module Component3 exposing (Model, Msg, init, update, view)
+
+import Html as H exposing (Html, Attribute)
+import Html.Attributes as HA
+import Html.Events as HE
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+type Msg
+    = Notify
+    | Increment
+
+
+update : Msg -> Model -> ( Model, Cmd Msg, Bool )
+update msg model =
+    case msg of
+        Increment ->
+            ( model + 1, Cmd.none, False )
+
+        Notify ->
+            ( model, Cmd.none, True )
+
+
+view : Model -> Html Msg
+view model =
+    H.div []
+        [ H.button [ HE.onClick Increment ] [ H.text "+" ]
+        , H.div [ countStyle ] [ H.text (toString model) ]
+        , H.button [ HE.onClick Notify ] [ H.text "Notify" ]
+        ]
+
+
+countStyle : Attribute Msg
+countStyle =
+    HA.style
+        [ ( "font-size", "20px" )
+        , ( "font-family", "monospace" )
+        , ( "display", "inline-block" )
+        , ( "width", "50px" )
+        , ( "text-align", "center" )
+        ]
--- Main.elm
+++ Main.elm
@@ -5,6 +5,7 @@ import Html.App
 import Html.Attributes as HA
 import Component1
 import Component2
+import Component3
 
 
 type alias Model =
@@ -12,12 +13,15 @@ type alias Model =
     , notified1 : Bool
     , component2 : Component2.Model
     , notified2 : Bool
+    , component3 : Component3.Model
+    , notified3 : Bool
     }
 
 
 type Msg
     = Component1Msg Component1.Msg
     | Component2Msg Component2.Msg
+    | Component3Msg Component3.Msg
 
 
 main : Program Never
@@ -32,7 +36,7 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model Component1.init False Component2.init False
+    ( Model Component1.init False Component2.init False Component3.init False
     , Cmd.none
     )
 
@@ -65,6 +69,17 @@ update msg model =
                         { model | component2 = c2model } ! [ Cmd.map Component2Msg c2cmd ]
 
 
+        Component3Msg c3Msg ->
+            let
+                ( c3model, c3cmd, notified ) =
+                    Component3.update c3Msg model.component3
+                notified' =
+                    if notified then not model.notified3 else model.notified3
+            in
+                ( { model | component3 = c3model, notified3 = notified' }, Cmd.map Component3Msg c3cmd )
+            
+
+
 view : Model -> Html.Html Msg
 view model =
     Html.div []
@@ -72,6 +87,8 @@ view model =
             [ Component1.view model.component1 |> Html.App.map Component1Msg ]
         , Html.div [ HA.classList [ ( "notified", model.notified2 ) ] ]
             [ Component2.view model.component2 |> Html.App.map Component2Msg ]
+        , Html.div [ HA.classList [ ( "notified", model.notified3 ) ] ]
+            [ Component3.view model.component3 |> Html.App.map Component3Msg ]
         ]
 
 
